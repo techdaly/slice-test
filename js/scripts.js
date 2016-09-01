839c044a8682fe1dd25ae62b16c5541996dfa45d
Amanda Daly <techdaly@gmail.com>
2016-09-01 09:47:16 -0700

    Initial Commit

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+console.log("Welcome to Slice Test");
+
+
+$('#button-1').click(function(){
+  $("#row-1").toggleClass('done');
+});
+
+$('#button-2').click(function(){
+  $("#row-2").toggleClass('done');
+});
+
+$('#button-3').click(function(){
+  $("#row-3").toggleClass('done');
+});
+
+$('#button-4').click(function(){
+  $("#row-4").toggleClass('done');
+});
+
+$('#button-5').click(function(){
+  $("#row-5").toggleClass('done');
+});
